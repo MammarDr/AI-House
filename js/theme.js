2ab8c92aec4916ca755c74990159332d268f567e
MammarDr <140546722+MammarDr@users.noreply.github.com>
2026-03-07 17:22:32 +0100

    New Css Styles

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,21 @@
-const themeToggle = document.getElementById("theme");
+function saveThemePreference(theme) {
+  localStorage.setItem("theme", theme);
+}
 
-themeToggle.addEventListener("click", () => {
-  document.documentElement.classList.toggle("dark");
+const themeBtn = document.getElementById("theme");
+const documentElement = document.documentElement;
+
+if (localStorage.getItem("theme") === "dark") {
+  documentElement.classList.add("dark");
+} else {
+  documentElement.classList.remove("dark");
+}
+
+themeBtn.addEventListener("click", () => {
+  documentElement.classList.toggle("dark");
+  if (documentElement.classList.contains("dark")) {
+    saveThemePreference("dark");
+  } else {
+    saveThemePreference("light");
+  }
 });
--- js/tracker.js
+++ js/tracker.js
@@ -1,18 +1,16 @@
-function updatetracker(){
+function updatetracker() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let id = document.getElementById("studentid").value;
+  let dept = document.getElementById("department").value;
 
-let name=document.getElementById("name").value
-let email=document.getElementById("email").value
-let id=document.getElementById("studentid").value
-let dept=document.getElementById("department").value
+  let step1 = document.getElementById("dot1");
+  let step2 = document.getElementById("dot2");
+  let step3 = document.getElementById("dot3");
+  let step4 = document.getElementById("dot4");
 
-let step1=document.getElementById("dot1")
-let step2=document.getElementById("dot2")
-let step3=document.getElementById("dot3")
-let step4=document.getElementById("dot4")
-
-step1.style.background=name ? "#027e02" : "#1b2742"
-step2.style.background=email ? "#FF7D04" : "#1b2742"
-step3.style.background=id ? "#027e02" : "#1b2742"
-step4.style.background=dept ? "#FF7D04" : "#111827"
-
+  step1.style.background = name ? "#027e02" : "#1b2742";
+  step2.style.background = email ? "#FF7D04" : "#1b2742";
+  step3.style.background = id ? "#027e02" : "#1b2742";
+  step4.style.background = dept ? "#FF7D04" : "#111827";
 }
